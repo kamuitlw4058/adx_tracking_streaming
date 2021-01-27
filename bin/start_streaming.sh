7ca26b2357d552428bf9eea3e2e7295067df8300
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-01-27 13:55:01 +0800

    update

--- bin/start_streaming.sh
+++ bin/start_streaming.sh
@@ -2,9 +2,9 @@ base_path=$(cd `dirname $0`; cd ..; pwd)
 cd $base_path
 export SPARK_CLASSPATH=$base_path/lib:$SPARK_CLASSPATH
 datetime=`date '+%Y%m%d%H%M%S'`
-/opt/spark-2.4.5-hadoop3.1-1.0.2/bin/spark-submit \
+nohup /opt/spark-2.4.5-hadoop3.1-1.0.2/bin/spark-submit \
                 --jars /opt/spark_jars/spark-streaming-kafka-0-10_2.11-2.4.5.jar,/opt/spark_jars/protobuf-java-3.8.0.jar,/opt/spark_jars/kafka-clients-0.10.2.2.jar,/opt/spark_jars/adp_common-1.0.jar \
-                target/AdposEventsMerger-1.0-SNAPSHOT.jar
+                target/AdposEventsMerger-1.0-SNAPSHOT.jar > logs/spark.log & 
  
                 
 
