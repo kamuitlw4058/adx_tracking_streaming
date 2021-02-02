97e300e7e77591e4967c1df4dd774c2c6099cb13
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-02-02 14:13:22 +0800

    update

--- bin/monitor_streaming.sh
+++ bin/monitor_streaming.sh
@@ -6,7 +6,7 @@ pid=`ps -ef | grep $app | grep -v grep | awk '{print $2}'`
 if  [ ! -n "$pid" ] ;then
     echo "$cur_time 服务进程失败重启进程..."
     echo "restart at $cur_time" > logs/spark/restart.log
-    cp logs/spark/spark.log logs/spark/spark_${cur_time}.log
+    cp logs/spark/spark.log logs/spark/tmp/spark_${cur_time}.log
     bin/start_streaming.sh
 else
     echo "$cur_time 服务运行正常"
