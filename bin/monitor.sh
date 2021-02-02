7812d6d96dba9b4da0358eb8c9efe13e8c4e1ce7
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-02-02 13:57:21 +0800

    update

---
+++ bin/monitor.sh
@@ -0,0 +1,10 @@
+base_path=$(cd `dirname $0`; cd ..; pwd)
+cd $base_path
+
+pid=`ps -ef | grep HiveServer2 | grep -v grep | awk '{print $2}'`
+if  [ ! -n "$pid" ] ;then
+    then
+        echo "服务进程失败重启进程"
+    else
+        echo "服务运行正常"
+fi
