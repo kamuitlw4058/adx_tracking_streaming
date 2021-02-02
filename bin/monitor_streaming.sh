efc55d90cc57bdde6a00120bbdd4314a3fad523f
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-02-02 14:10:41 +0800

    update

--- bin/monitor_streaming.sh
+++ bin/monitor_streaming.sh
@@ -4,10 +4,10 @@ app="AdposEventMergerStreaming"
 cur_time=$(date +%Y%m%d%H%M)
 pid=`ps -ef | grep $app | grep -v grep | awk '{print $2}'`
 if  [ ! -n "$pid" ] ;then
-    echo "服务进程失败重启进程..."
+    echo "$cur_time 服务进程失败重启进程..."
     echo "restart at $cur_time" > logs/spark/restart.log
     cp logs/spark/spark.log logs/spark/spark_${cur_time}.log
     bin/start_streaming.sh
 else
-    echo "服务运行正常"
+    echo "$cur_time 服务运行正常"
 fi
