531877077ce6eeb0843789712fefcfc4e99eea23
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-02-02 13:59:59 +0800

    update

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -1,10 +1,9 @@
 base_path=$(cd `dirname $0`; cd ..; pwd)
 cd $base_path
-
+app="AdposEventMergerStreaming"
 pid=`ps -ef | grep HiveServer2 | grep -v grep | awk '{print $2}'`
 if  [ ! -n "$pid" ] ;then
-    then
-        echo "服务进程失败重启进程"
-    else
-        echo "服务运行正常"
+    echo "服务进程失败重启进程"
+else
+    echo "服务运行正常"
 fi
