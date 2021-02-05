f4fd88ed4d157fe7a47a1fef6855fe53a157df2f
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-02-05 15:27:47 +0800

    update

--- src/main/python/scan_pb.py
+++ src/main/python/scan_pb.py
@@ -2,6 +2,7 @@ from datetime import datetime,timedelta
 
 from python_common.utils.datetime_utils import  str_to_datetime
 from python_common.io.scan_datafiles import ScanDataFiles,true_filter,false_filter
+from python_common.utils.shell_utils import run_cli
 
 from datetime import datetime,timedelta
 
@@ -29,6 +30,12 @@ def delete_file_filter(filepath,filename,dir_list,extend_params):
 
 def process_func(data_file,extend_params,data_file_logger):
     data_file_logger.info(f'data_file:{data_file}, extend_params:{extend_params}')
+    pb_path = f'/home/wls81/workspace/kimi/adp_common/src/main/proto/adx_adpos_events.proto'
+    pb_class = 'AdxAdposEvents'
+    db = 'ad_adx'
+    table = 'adpos_events'
+    cmd_format = f"""cat {data_file} | clickhouse-client -h   cc-uf6y3p4u3ff10s973.ads.aliyuncs.com -u xnad_suanfa  --password M6119Hb#Aj80TtosdkjUDN89 --port 3306 --query "INSERT INTO {db}.{table} FORMAT Protobuf SETTINGS format_schema='{pb_path}:{pb_class}'" """
+    r =  run_cli(cmd_format)
     return True
 
 
