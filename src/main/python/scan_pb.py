f66bf2257b936bd861a3f91bc5257ea3d49a9a7f
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-02-05 15:12:48 +0800

    update

--- src/main/python/scan_pb.py
+++ src/main/python/scan_pb.py
@@ -21,9 +21,9 @@ def delete_file_filter(filepath,filename,dir_list,extend_params):
             file_create_dt =  datetime.strptime(str(filename.split('-')[0]), '%Y%m%d%H%M%S')
             delete_dt = datetime.now() - timedelta(minutes=30)
             if file_create_dt < delete_dt:
-                print(f'will delete  file filter:{filepath}')
+                return True
         except Exception as e:
-            print(f'process delete failed!{e}')
+            print(f'process delete file failed!{e}')
     return False
 
 
